12429907c44a5cffa27fa67421158aa3063165ef
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-11-13 06:09:11 +0530

    Update StickHeroPlayer.java

--- game/src/application/StickHeroPlayer.java
+++ game/src/application/StickHeroPlayer.java
@@ -1,5 +1,8 @@
 package application;
+
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.input.KeyCode;
 
 public class StickHeroPlayer {
     private double x;
@@ -9,30 +12,94 @@ public class StickHeroPlayer {
     private int cherriesCollected;
     private int revivalsLeft;
     private int score;
+    private double extensionStartTime; // Variable to store the time when stick extension starts
+    private boolean isFlipping; // Variable to track if the player is flipping
+    private double flipStartTime; // Variable to store the time when flipping starts
+
 
     public StickHeroPlayer(double x, double y) {
-        this.setX(x);
-        this.setY(y);
-        this.setStickLength(0);
+        this.x = x;
+        this.y = y;
+        this.stickLength = 0;
         this.isExtending = false;
-        this.setCherriesCollected(0);
+        this.cherriesCollected = 0;
         this.revivalsLeft = 3; // or any desired number
-        this.setScore(0);
+        this.score = 0;
+        this.extensionStartTime = 0;
+        this.isFlipping = false;
+        this.flipStartTime = 0;
     }
-
-    public void update(double deltaTime) {
-        // Update player's position and other game logic here
+//    isFlipping and flipStartTime variables to track the flipping state and the time when flipping starts. The update method checks for user input to initiate flipping and calculates the flip time when flipping is in progress. If the flip time falls within a certain range (e.g., between 0.2 and 1.0 seconds), it considers it a successful flip and calls the collectCherry method
+    public void update(double deltaTime, PlatformManager platformManager) {
         if (isExtending) {
+        	if (extensionStartTime == 0) {
+                // Start timing when stick extension begins
+                extensionStartTime = System.nanoTime() * 1e-9; // Convert nanoseconds to seconds
+            }
+
             // If the stick is extending, update the stick length or handle stick animation
             stickLength = calculateStickLength(); // Update stick length
         } else {
             // Handle player's movement when not extending the stick
             // Check for collisions with platforms or other game objects
+            checkCollision(platformManager);
+
+            // Check for user input to initiate flipping
+            if (isFlipping && flipStartTime == 0) {
+                // Start timing when flipping begins
+                flipStartTime = System.nanoTime() * 1e-9; // Convert nanoseconds to seconds
+            }
+            if (isFlipping && flipStartTime > 0) {
+                double flipTime = System.nanoTime() * 1e-9 - flipStartTime;
+                // Check if the flip time is within a certain range for successful flip
+                if (flipTime >= 0.2 && flipTime <= 1.0) {
+                    collectCherry(); // Collect cherry when successfully flipped
+                }
+                // Reset flipping variables for the next flip
+                isFlipping = false;
+                flipStartTime = 0;
+            }
         }
     }
 
+    // Method to handle user input for flipping
+    public void flip() {
+        isFlipping = true;
+    }
 
-//    this implementation, we check if the player is not already extending the stick, and if not, we set the isExtending flag to true to indicate that the stick is being extended. You should handle the timing of stick extension according to your game's rules.
+    private void checkCollision(PlatformManager platformManager) {
+        // Check collision with platforms and handle accordingly
+        for (Platform platform : platformManager.getPlatforms()) {
+            if (platform.isCollision(x, y, stickLength, getPlayerDimensions())) {
+                // Successful landing on a platform
+                double landingTime = System.nanoTime() * 1e-9; // Convert nanoseconds to seconds
+                double extensionTime = landingTime - extensionStartTime;
+
+                // Check if the extension time is within a certain range for successful landing
+                if (extensionTime >= 0.5 && extensionTime <= 2.0) {
+                    score += 100; // Award points for successful landing
+                } else {
+                    // Failed to time the stick extension correctly, end the game
+                    // You may want to reset the game or show a game over screen
+                    // Handle the game over logic as needed
+                }
+                // Reset timing variables for the next extension
+                extensionStartTime = 0;
+                return;
+            }
+        }
+
+        // Failed to land on a platform, end the game or initiate reviving logic
+        if (revivalsLeft > 0) {
+            // Placeholder for reviving logic
+            isExtending = true; // Extend the stick for reviving
+        } else {
+            // Game over logic
+            // You may want to reset the game or show a game over screen
+            // Handle the game over logic as needed
+        }
+    }
+
     public void extendStick() {
         if (!isExtending) {
             // Start extending the stick
@@ -40,7 +107,6 @@ public class StickHeroPlayer {
         }
     }
 
-//    The releaseStick() method is used to release the stick when the player releases the key or control.
     public void releaseStick() {
         if (isExtending) {
             // Release the stick and calculate the length
@@ -48,6 +114,8 @@ public class StickHeroPlayer {
             isExtending = false;
         }
     }
+
+
 
     private double calculateStickLength() {
         // Implement your logic to calculate the stick length here
@@ -57,12 +125,17 @@ public class StickHeroPlayer {
         return Math.min(100, stickLength + 1); // Increment stick length
     }
 
-
-
-
     public void collectCherry() {
-        setCherriesCollected(getCherriesCollected() + 1);
-        setScore(getScore() + 100); // Award points for collecting cherries
+        cherriesCollected++;
+        int cherryScore = calculateCherryScore();
+        score += cherryScore; // Award points for collecting cherries based on score calculation
+    }
+
+    private int calculateCherryScore() {
+        // Implement your scoring logic based on cherries collected
+        // Adjust the formula based on how you want to encourage higher scores
+        // This is just a placeholder, you can make it more complex or dynamic
+        return cherriesCollected * 100;
     }
 
     public boolean canRevive() {
@@ -72,65 +145,78 @@ public class StickHeroPlayer {
     public void revive() {
         if (canRevive()) {
             revivalsLeft--;
-            setCherriesCollected(getCherriesCollected() - 10); // Deduct cherries for revival
+            cherriesCollected -= 10; // Deduct cherries for revival
             // Implement the revival logic
+            isExtending = false; // Reset the extending flag after revival
         }
     }
 
     public void draw(GraphicsContext gc) {
         // Draw the player and stick
-        // Use gc.strokeLine() to draw the stick
-        // Implement player animations
+        gc.setFill(Color.BLUE); // Set player color (replace with your player color)
+        gc.fillOval(x - 5, y - 5, 20, 40); // Draw player (replace with your player shape)
+
+        // Draw the stick
+        gc.setStroke(Color.BROWN); // Set stick color
+        gc.setLineWidth(5); // Set stick width
+        gc.strokeLine(x, y, x, y - stickLength); // Draw the stick
     }
 
-	public double getX() {
-		return x;
-	}
+    public double getX() {
+        return x;
+    }
 
-	public void setX(double x) {
-		this.x = x;
-	}
+    public void setX(double x) {
+        this.x = x;
+    }
 
-	public double getY() {
-		return y;
-	}
+    public double getY() {
+        return y;
+    }
 
-	public void setY(double y) {
-		this.y = y;
-	}
+    public void setY(double y) {
+        this.y = y;
+    }
 
-	public double getPlayerDimensions() {
-	    // Assuming the player's character is not a circle but a human-like shape
-	    // Use dimensions that approximate the player's shape
-	    double playerWidth = 30.0;  // Replace with the actual width of the hitbox
-	    double playerHeight = 60.0;  // Replace with the actual height of the hitbox
+    public double getStickLength() {
+        return stickLength;
+    }
 
-	    // Return the maximum dimension (radius) to represent the player's hitbox
-	    return Math.max(playerWidth, playerHeight) / 2;
-	}
+    public void setStickLength(double stickLength) {
+        this.stickLength = stickLength;
+    }
 
-	public int getCherriesCollected() {
-		return cherriesCollected;
-	}
+    public int getCherriesCollected() {
+        return cherriesCollected;
+    }
 
-	public void setCherriesCollected(int cherriesCollected) {
-		this.cherriesCollected = cherriesCollected;
-	}
+    public void setCherriesCollected(int cherriesCollected) {
+        this.cherriesCollected = cherriesCollected;
+    }
 
-	public int getScore() {
-		return score;
-	}
+    public int getRevivalsLeft() {
+        return revivalsLeft;
+    }
 
-	public void setScore(int score) {
-		this.score = score;
-	}
+    public void setRevivalsLeft(int revivalsLeft) {
+        this.revivalsLeft = revivalsLeft;
+    }
 
-	public double getStickLength() {
-		return stickLength;
-	}
+    public int getScore() {
+        return score;
+    }
 
-	public void setStickLength(double stickLength) {
-		this.stickLength = stickLength;
-	}
+    public void setScore(int score) {
+        this.score = score;
+    }
 
+    public double getPlayerDimensions() {
+        // Assuming the player's character is not a circle but a human-like shape
+        // Use dimensions that approximate the player's shape
+        double playerWidth = 30.0;  // Replace with the actual width of the hitbox
+        double playerHeight = 60.0;  // Replace with the actual height of the hitbox
+
+        // Return the maximum dimension (radius) to represent the player's hitbox
+        return Math.max(playerWidth, playerHeight) / 2;
+    }
 }
